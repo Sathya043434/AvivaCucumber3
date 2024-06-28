36a0dced1e43c716bdeae0d1ee531cd75b15832a
Satya <satyanarayanajogu04@gmail.com>
2024-06-28 18:01:14 +0530

    Satya branch Syso 4

--- src/main/java/Sample/Sample.java
+++ src/main/java/Sample/Sample.java
@@ -2,7 +2,13 @@ package Sample;
 
 public class Sample {
 
-  System.out.println("Hello world");
+  public static void Setup (){
+
+    System.out.println("Satya branch - Sample 1");
+    System.out.println("Satya branch - Sample 2");
+    System.out.println("Satya branch - Sample 3");
+    System.out.println("Satya branch - Sample 4");
+  }
 
 
 }
--- src/main/java/Sample/example.java
+++ src/main/java/Sample/example.java
@@ -8,14 +8,10 @@ public class example {
 
     @BeforeClass
     public static void setUp() {
-        System.out.println("Hello world")
-
-    } 
-
-    @AfterClass
-    public static void tearDown() {
-
+        System.out.println("Satya branch - Example 1");
+        System.out.println("Satya branch - Example 2");
+        System.out.println("Satya branch - Example 3");
+        System.out.println("Satya branch - Example 4");
 
     }
-
 }
