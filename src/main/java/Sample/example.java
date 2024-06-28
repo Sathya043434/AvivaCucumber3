491a953a6d9a6a81b031c4ca4920020bdb540982
Satya <satyanarayanajogu04@gmail.com>
2024-06-28 09:15:07 +0530

    Message 2

--- src/main/java/Sample/example.java
+++ src/main/java/Sample/example.java
@@ -1,4 +1,20 @@
 package Sample;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
 public class example {
+
+
+    @BeforeClass
+    public static void setUp() {
+
+    }
+
+    @AfterClass
+    public static void tearDown() {
+
+
+    }
+
 }
