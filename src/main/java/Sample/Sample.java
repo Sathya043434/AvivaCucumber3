2fa503e945467c8464a0492334e4d10afd12b633
Sathya043434 <149290184+Sathya043434@users.noreply.github.com>
2024-07-02 07:09:41 +0530

    Update Sample.java

--- src/main/java/Sample/Sample.java
+++ src/main/java/Sample/Sample.java
@@ -8,6 +8,7 @@ public class Sample {
         System.out.println("Satya2 branch - Sample 3");
         System.out.println("Satya2 branch - Sample 4");
         System.out.println("Satya2 branch - Sample 5");
+        System.out.println("Satya2 branch - Sample 6");
 
 
     }
